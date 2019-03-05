8178384ce7932cabf9f78fac2734dde9f92bdb51
Scott Schindler <scottschindler29@gmail.com>
2019-03-04 20:59:48 -0500

    starting mobile responsiveness

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // JS goes here
+
+class Dropdown {
+    constructor(element){
+        this.element = element;
+
+        this.element.button.addEventListener('click', () => {console.log('clicked!')})
+
+    }
+}
+
+
+
+const burger = document.querySelector(".hamburger");
+
+
+
+burger.addEventListener('click', () => {
+  console.log(`clicked!`);
+  event.stopPropagation();
+})
